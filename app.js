49cd0df473ecccf324ff5f938bfe139da4dbc191
Jacqueline-dev <jacque.ferreira@outlook.com.br>
2022-02-24 20:50:56 -0300

    estizando a página

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const main = document.querySelector('main')
 const buttonInsertText = document.querySelector('.btn-toggle') 
+const buttonReadText = document.querySelector('#read')
 const divTextBox = document.querySelector('.text-box')
 const closeDivTextBox = document.querySelector('.close')
+const selectElement = document.querySelector('select')
+const textArea = document.querySelector('textArea')
 
 const humanExpressions = [
   {img: './img/drink.jpg', text: 'Estou com sede'},
@@ -19,6 +22,21 @@ const humanExpressions = [
   
 ]
 
+const utterance = new SpeechSynthesisUtterance()
+const setTextMessage = text => {
+  utterance.text = text
+}
+
+const speakText = () => {
+  speechSynthesis.speak(utterance)
+}
+
+const setVoice = event => {
+ const selectedVoice = voices .find(voice => voice.name === event.target.value)
+ utterance.voice = selectedVoice
+}
+
+
 const createExpressionBox = ({img, text})=>{
   const div = document.createElement('div');
 
@@ -28,15 +46,49 @@ const createExpressionBox = ({img, text})=>{
    <p class="info">${text} </p>
   `
 
+div.addEventListener('click', () => {
+  setTextMessage(text)
+  speakText()
 
+  div.classList.add('active')
+  setTimeout(() => {
+    div.classList.remove('active')
+  }, 1000)
+})
 
-  main.appendChild(div)
+  main.appendChild(div)   
 
 }
 humanExpressions.forEach(createExpressionBox)
 
 let voices = []
-console.log(speechSyncthesis)
+
+speechSynthesis.addEventListener('voiceschanged', () => {
+  voices =  speechSynthesis.getVoices()
+  const googleVoice = voices.find(voice => 
+    voice.name === 'Google português do Brasil')
+  const  microsoftVoice =  voices.find(voice => 
+    voice.name === 'Microsoft Maria Desktop - Portuguese(Brazil)')
+
+    if (googleVoice) {
+      utterance.voice = googleVoice
+
+    } else if (microsoftVoice ) {
+      utterance.voice = microsoftVoice 
+    }
+
+    
+  voices.forEach(({name, lang}) => {
+    const option = document.createElement('option')
+
+    option.value = name
+    option.text = `${lang} | ${name}`
+    selectElement.appendChild(option)
+    
+  })
+
+  
+})
 
 buttonInsertText.addEventListener('click', () => {
   divTextBox.classList.add('show')
@@ -45,4 +97,12 @@ buttonInsertText.addEventListener('click', () => {
 
 closeDivTextBox.addEventListener('click', () => {
   divTextBox.classList.remove('show')
+})
+
+selectElement.addEventListener('change', setVoice)
+
+buttonReadText.addEventListener('click', () => {
+  setTextMessage(textArea.value)
+  speakText()
+  
 })
